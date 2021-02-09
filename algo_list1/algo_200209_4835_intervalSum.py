b4858715f32b8df3235b80c4661b7a399d349f40
DESKTOP-EF9G313\Woonghyun <tofan@naver.com>
2021-02-09 22:27:22 +0900

    test

--- algo_list1/algo_200209_4835_intervalSum.py
+++ algo_list1/algo_200209_4835_intervalSum.py
@@ -1,15 +1,19 @@
-t_num = int(input())
+t_num = int(input()) # 테스트할 횟수
 for tc in range(1, t_num + 1):
+    
     info = list(map(int, input().split()))
-    cnt = info[0] # 숫자의 개수
-    interval = info[1] # 구간
-
+    length = info[0] # 정수 list의 길이를 의미한다.  
+    interval = info[1] # 더할 구간의 크기를 의미한다. 
+    
+    # 정수 list를 입력받는다.
     numbers = list(map(int, input().split()))
-    result = []
-
-    for idx in range(0,cnt-interval+1):
+    result = [] # 정수 list에서 나올 수 있는 모든 구간합을 저장할 list이다.
+    # list의 크기가 5이고, interval의 크기가 3인 경우에는 3번만 구간합을 구하면 된다.
+    # 3 = (list의 크기) - interval + 1
+    for idx in range(0,length - interval + 1):
+        # 각 구간합을 저장할 변수를 정의한다.
         interval_sum = 0
-        for number in numbers[idx:interval+idx]:
+        for number in numbers[idx:interval+idx]: # a[0:2]와 같이 구간을 선택하면 index가 2인 element는 선택되지 않는다.
             interval_sum += number
 
         result.append(interval_sum)
--- algo_study/algo_1204_mode.py
+++ algo_study/algo_1204_mode.py
@@ -1,21 +1,36 @@
 # count 하는 배열을 만든다.
 # 0점부터 100점까지 101의 길이를 갖는다.
-n = int(input())
+n = int(input()) # 테스트 회수
 for tc in range(0, n):
+    # 시험 점수의 범위가 0 ~ 100점까지이다.
     score_cnt = [0] * 101
-    test_case_index = int(input()) # 테스트 케이스 번호를 의미한다.
+    tc_index = int(input()) # 테스트 케이스 번호. #1, #2, #3...
+
     scores = list(map(int, input().split()))
-    # 이 때 학생수가 1000명이므로, scores 역시 1000개이다.
+    # 이 때 학생수가 1000명이므로, scores 역시 1000개의 element를 갖는 list이다.
     for score in scores:
-        score_cnt[score] += 1
+        score_cnt[score] += 1 # 값에 해당하는 인덱스에 +1씩 카운트한다.
 
+    # score_cnt : index는 점수를 나타내고, index에 해당하는 값은 count를 나타낸다.
     # 최빈값을 찾는다.
-    max_val = score_cnt[0]
-    max_idx = 0
-    for i, cnt in enumerate(score_cnt):
-        if cnt > max_val:
-            max_val = cnt
-            max_idx = i
+    most_cnt = score_cnt[0]
+    most_score = 0 # 이게 결국 점수를 의미한다.
 
-    # 최빈수가 여러 개 일때를 고려해야 한다...
-    print(f'#{test_case_index} {score_cnt.index(max_val)}')
+    for score,cnt in enumerate(score_cnt):
+        if cnt > most_cnt:
+            most_cnt = cnt
+            most_score = score
+        # 최빈수가 여러 개 일때를 고려해야 한다...
+        # 카운트한 값이 동일한 경우에는? 큰 값을 기준으로 넣어야 한다.
+        if cnt == most_cnt:
+            # 값을 비교해야 한다.
+            if most_score > score:
+                pass
+            else:
+                most_cnt = cnt
+                most_score = score
+
+    print(f'#{tc_index} {most_score}')
+
+    # list comprehension을 이용하여 다음과 같이 작성할 수도 있다.
+    # ans = [i for i in range(len(arr)) if arr[i] == max(arr)]
