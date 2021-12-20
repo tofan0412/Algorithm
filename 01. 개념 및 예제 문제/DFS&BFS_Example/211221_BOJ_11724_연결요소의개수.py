1c53944f80de1c488fe84f65e4aa5ad2652354d1
DESKTOP-R3600\tofan <tofan12312@gmail.com>
2021-12-21 01:17:29 +0900

    211221 BOJ 11724 SILVER2 연결요소의개수 PASS

---
+++ 01. 개념 및 예제 문제/DFS&BFS_Example/211221_BOJ_11724_연결요소의개수.py
@@ -0,0 +1,35 @@
+'''
+방향 없는 그래프가 주어졌을 때, 연결 요소(Connected Component)의 개수를 구하는
+프로그램을 작성하시오
+'''
+
+def dfs(node):
+    global visited
+    stack = []
+    stack.append(node)
+    visited[node] = 1
+    while stack:
+        now = stack.pop()
+        for adj_node in adj[now]:
+            if visited[adj_node] == 0:
+                stack.append(adj_node)
+                visited[adj_node] = 1
+    return 1
+
+N, M = map(int, input().split()) # 정점의 개수 M과 간선의 개수 M
+adj = [[] for _ in range(N+1)] # 0번 노드는 없으므로
+visited = [0] * (N + 1)
+result = 0
+
+for i in range(M):
+    u, v = map(int, input().split())
+    adj[u].append(v)
+    adj[v].append(u)
+
+# 1번부터 출발하자.
+for node in range(1, N+1):
+    # 방문한 적이 없다면
+    if visited[node] == 0:
+        result += dfs(node)
+
+print(result)
