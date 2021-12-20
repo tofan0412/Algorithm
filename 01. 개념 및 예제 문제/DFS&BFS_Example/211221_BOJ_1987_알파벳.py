3996db585aa5514bc4f20b109f636eb8bdcb081d
DESKTOP-R3600\tofan <tofan12312@gmail.com>
2021-12-21 04:08:14 +0900

    211221 BOJ 2468 GOLD4 알파벳 풀이중

--- 01. 개념 및 예제 문제/DFS&BFS_Example/211221_BOJ_1987_알파벳.py
+++ 01. 개념 및 예제 문제/DFS&BFS_Example/211221_BOJ_1987_알파벳.py
@@ -1,1 +1,14 @@
 # GOLD4
+def solution(arr, point):
+
+
+R, C = map(int, input().split())
+arr = [[]*C for _ in range(R)]
+
+for i in range(R):
+    arr[i] = list(input())
+
+# 사방 탐색. 완전 탐색
+for row in range(R):
+    for col in range(C):
+        solution([row, col])
