5d3deea014b44d8967b43783248b5d1b7e82bb5c
DESKTOP-R3600\tofan <tofan12312@gmail.com>
2021-12-28 23:32:59 +0900

    211228 BOJ 2750 BRONZE1 수정렬하기 PASS

--- 01. 개념 및 예제 문제/DFS&BFS_Example/211227_BOJ_2750_수정렬하기.py
+++ 01. 개념 및 예제 문제/DFS&BFS_Example/211227_BOJ_2750_수정렬하기.py
@@ -7,14 +7,30 @@ for _ in range(N):
 
 # 1. 선택 정렬을 이용하여 오름차순으로 정렬하세요.
 # 선택정렬 :  1 ~ N-1번째 수를 탐색하여 최소값을 0번째 인덱스에, 2 ~ N-1번째 수를 탐색하여 최소값을 1번째 인덱스에, ...
-for i in range(N):
-    min_index = i
-    for j in range(i+1, N):
-
-
-
+# for i in range(N):
+#     min_index = i
+#     for j in range(i+1, N):
+#         if arr[j] < arr[min_index]:
+#             min_index = j
+#     arr[min_index], arr[i] = arr[i], arr[min_index]
 
 # 2. 버블 정렬을 이용하여 오름차순으로 정렬하세요.
+# for i in range(N):
+#     for j in range(0, N-i-1):
+#         if arr[j] > arr[j+1]:
+#             arr[j], arr[j+1] = arr[j+1], arr[j]
+
 
 # 3. 삽입 정렬을 이용하여 오름차순으로 정렬하세요.
+# 삽입 정렬 : 첫번째 원소는 정렬이 완료된 원소라 가정한다.
+for i in range(N-1):
+    for j in range(i+1, 0, -1):
+        # 왼쪽에 있는 원소보다 오른쪽에 있는 원소가 작다면 순서를 바꿔준다.
+        if arr[j] < arr[j-1]:
+            arr[j], arr[j-1] = arr[j-1], arr[j]
+        # 그렇지 않다면 넘어간다.
+        else:
+            break
 
+for number in arr:
+    print(number)
