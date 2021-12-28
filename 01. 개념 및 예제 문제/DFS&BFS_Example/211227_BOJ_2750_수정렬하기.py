71470dabfef9e12a9176314818752d906fd2e854
cho woonghyun <tofan12312@gmail.com>
2021-12-28 23:08:45 +0900

    211228 BOJ 2750 BRONZE1 수정렬하기 풀이중

--- 01. 개념 및 예제 문제/DFS&BFS_Example/211227_BOJ_2750_수정렬하기.py
+++ 01. 개념 및 예제 문제/DFS&BFS_Example/211227_BOJ_2750_수정렬하기.py
@@ -1,1 +1,20 @@
 # BRONZE1
+N = int(input())
+arr = []
+for _ in range(N):
+    number = int(input())
+    arr.append(number)
+
+# 1. 선택 정렬을 이용하여 오름차순으로 정렬하세요.
+# 선택정렬 :  1 ~ N-1번째 수를 탐색하여 최소값을 0번째 인덱스에, 2 ~ N-1번째 수를 탐색하여 최소값을 1번째 인덱스에, ...
+for i in range(N):
+    min_index = i
+    for j in range(i+1, N):
+
+
+
+
+# 2. 버블 정렬을 이용하여 오름차순으로 정렬하세요.
+
+# 3. 삽입 정렬을 이용하여 오름차순으로 정렬하세요.
+
