bb806aee07728319a41bbb664ba7a2f6ed32c1e6
cho woonghyun <tofan12312@gmail.com>
2022-01-07 19:54:54 +0900

    220107 BOJ 2805 SILVER3 나무자르기 PASS

--- 01. 개념 및 예제 문제/BinarySearch_Example/220107_BOJ_2805_나무자르기.py
+++ 01. 개념 및 예제 문제/BinarySearch_Example/220107_BOJ_2805_나무자르기.py
@@ -1,67 +1,32 @@
 # SILVER3
-# N은 1 ~ 백만, M은 1에서 20억 사이이다.
-def merge_sort(arr):
-    if len(arr) <= 1:
-        return arr
-    mid = len(arr) // 2
+def binary_search(arr, max_h, min_h, M):
+    global result
+    sum_of_tree = 0
+    if max_h < min_h: # 같은 경우에도 높이 4로 컷할 수 있으므로 같은 경우는 포함되어선 안된다.
+        return
 
-    left = merge_sort(arr[:mid])
-    right = merge_sort(arr[mid:])
-    merged = []
+    mid = (max_h + min_h) // 2
 
-    i, j = 0, 0
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            merged.append(left[i])
-            i += 1
-        else:
-            merged.append(right[j])
-            j += 1
+    for i in range(len(arr)):
+        if arr[i] - mid >= 0:
+            sum_of_tree += arr[i] - mid
 
-    if i == len(left):
-        while j < len(right):
-            merged.append(right[j])
-            j += 1
-    if j == len(right):
-        while i < len(left):
-            merged.append(left[i])
-            i += 1
-    return merged
+    if sum_of_tree >= M:
+        result = mid
 
-
-def binary_search(arr, H, start, end):
-    if start > end:
-        return None
-    mid = (start + end) // 2
-
-    if arr[mid] == H or arr[mid] > H:
-        return mid
-    elif arr[mid] < H:
-        return binary_search(arr, H, mid + 1, end)
-    elif arr[mid] > H:
-        return binary_search(arr, H, start, mid - 1)
+    # 목적 : 최대 H를 구하는 것이다.
+    # 만약 자른 나무 길이의 합이 M보다 작다면? H를 낮춰야 한다.
+    if sum_of_tree < M:
+        # 이 때 H를 이진 탐색으로 하자.
+        return binary_search(arr, mid-1, min_h, M)
+    # 만약 자른 나무 길이의 합이 M보다 많거나 같다면? H를 높여야 한다.
+    elif sum_of_tree >= M:
+        return binary_search(arr, max_h, mid+1, M)
 
 
 N, M = map(int, input().split())
 height = list(map(int, input().split()))
-height = merge_sort(height)
 
-H = height[-1] - 1
-min_h = 0
-
-# 나무의 최대 개수는 최대 100만개이다.
-# 임의의 절단 높이 H에 대해, 절단한 후 나무 길이의 총합을 구하는 시간 복잡도는 O(N^2)이다.
-# 절단기에 설정할 수 있는 높이는 양의 정수 또는 0이다.
-for h in range(H, -1, -1):
-    i = binary_search(height, h, 0, len(height) - 1)
-
-    length = 0
-    for j in range(i, len(height)):
-        length += height[j] - h
-        # 더하면서 계산
-        if length >= M:
-            min_h = h
-            print(h)
-            break
-    if min_h != 0:
-        break
+result = 0
+binary_search(height, max(height), 0, M)
+print(result)
