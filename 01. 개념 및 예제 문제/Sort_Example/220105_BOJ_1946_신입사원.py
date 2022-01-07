eff3df1f2430d6fbd3de351a6a1439e474b7bd13
cho woonghyun <tofan12312@gmail.com>
2022-01-07 16:18:29 +0900

    220107 BOJ 1946 SILVER1 신입사원 PASS

--- 01. 개념 및 예제 문제/Sort_Example/220105_BOJ_1946_신입사원.py
+++ 01. 개념 및 예제 문제/Sort_Example/220105_BOJ_1946_신입사원.py
@@ -7,14 +7,20 @@ for tc in range(T):
         s_grade, i_grade = map(int, input().split())
         grades.append([s_grade, i_grade])
 
-    for (i, me) in enumerate(grades):
-        for (j, you) in enumerate(grades):
-            if me == you:
-                continue
-            if me[0] < you[0] and me[1] < you[1]:
-                grades.pop(j)
+    # 1. 서류 성적 순으로 정렬 한다.
+    grades = sorted(grades, key=lambda x:x[0])
 
-            if you[0] < me[0] and you[1] < me[1]:
-                grades.pop(i)
-                break
-    print(len(grades))
+    # 2. 뒤에서부터 내 앞사람들 중 최고 면접 성적과 내 면접 성적을 비교해서, 만약 내가 낮은 등수라면 나는 탈락
+    min_order = len(grades)
+    count = len(grades)
+    for me in grades:
+        if me[1] < min_order:
+            min_order = me[1]
+
+        if min_order < me[1]:
+            count -= 1
+    print(count)
+
+
+
+
