925c0f448c021083b5900d0f0ed7b6336be77547
DESKTOP-R3600\tofan <tofan12312@gmail.com>
2022-01-10 23:45:04 +0900

    220110 BOJ 10816 SILVER4 숫자카드2 풀이중

--- 01. 개념 및 예제 문제/Sort_Example/test.py
+++ 01. 개념 및 예제 문제/Sort_Example/test.py
@@ -1,45 +1,14 @@
-import sys
-
-
-def merge_sort(arr):
-    if len(arr) <= 1:
-        return arr
-
-    mid = len(arr) // 2
-
-    left = merge_sort(arr[:mid])
-    right = merge_sort(arr[mid:])
-
-    i = 0
-    j = 0
-
-    merged = []
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            merged.append(left[i])
-            i += 1
-        else:
-            merged.append(right[j])
-            j += 1
-
-    if i == len(left):
-        while j < len(right):
-            merged.append(right[j])
-            j += 1
-    if j == len(right):
-        while i < len(left):
-            merged.append(left[i])
-            i += 1
-    return merged
-
+def solution(N):
+    enable_print = 0
+    while N > 0:
+        if enable_print == 0 and N % 10 != 0: # 출력 불가능한 상태인데, 현재 자리수가 출력가능한 수라면 출력 가능하게 한다.
+            enable_print = 1
+        if enable_print == 1: # 출력 가능한 상태이면, 출력한다.
+            print(N % 10, end="")
+        N = N // 10
 
 N = int(input())
-array = []
+solution(N)
 
-for index in range(N):
-    array.append(int(input()))
-
-result = merge_sort(array)
-
-for num in result:
-    print(num)
+# 200과 같은 경우, 0이 출력되어선 안된다.
+# 10010과 같은 경우, 끝의 0은 출력되면 안되지만 1과 1 사이의 0은 출력되어야 한다.
