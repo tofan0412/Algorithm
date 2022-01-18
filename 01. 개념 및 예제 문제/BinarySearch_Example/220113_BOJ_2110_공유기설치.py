b12bc49401a8263e88bf382b4ded29a11cbf9cd4
cho woonghyun <tofan12312@gmail.com>
2022-01-18 14:47:57 +0900

    220118 BOJ 2110 GOLD5 공유기설치 PASS

--- 01. 개념 및 예제 문제/BinarySearch_Example/220113_BOJ_2110_공유기설치.py
+++ 01. 개념 및 예제 문제/BinarySearch_Example/220113_BOJ_2110_공유기설치.py
@@ -1,40 +1,37 @@
 # GOLD5
 # 참고 코드 : https://hongcoding.tistory.com/3
-def binary_search(arr, start, end):
-    global C, setup, min_dis
-
-    if start >= end or len(setup) == C:
-        return
-
-    mid = (start + end) // 2
-
-    # 만약 중간에 설치할 수 있다면 설치
-    if mid in coord and mid not in setup:
-        setup.append(mid)
-        binary_search(arr, arr[0], mid)
-    # 만약 설치할 수 없다면 : mid보다 더 먼 집에 설치할 수 있는지를 따져야 한다.
-    else:
-        # 1. mid보다 더 먼 집에 설치할 수 있는 경우
-        if arr[-1] > mid:
-            binary_search(arr, mid, end)
-        # 2. mid보다 더 먼 집에 설치할 수 없는 경우
-        else:
-            binary_search(arr, start, mid - 1)
-
-
-# N은 집의 개수, C는 공유기의 개수
 N, C = map(int, input().split())
-coord = [] # 집이 위치한 좌표 배열
-setup = [] # 공유기를 설치한 집의 배열
-for _ in range(N):
-    coord.append(int(input()))
 
-# 전제 조건. 이진탐색이므로 coord는 반드시 정렬되어야 한다.
-coord = sorted(coord)
-min_dis = 0
+array = []
+for i in range(N):
+    array.append(int(input()))
 
-setup.append(coord[0])
-binary_search(coord, coord[0], coord[-1])
-print(setup)
+array.sort()
 
-# 최대 거리값을 찾자.
+
+def binary_search(array, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        current = array[0] # 현재 설치한 집을 뜻한다.
+        count = 1 # 공유기를 설치한 집 수
+
+        # 두번째 집부터 마지막 집까지 점건한다.
+        for i in range(1, len(array)):
+            if array[i] >= current + mid:
+                count += 1
+                current = array[i]
+
+        if count >= C:
+            global answer
+            start = mid + 1
+            answer = mid
+        else:
+            end = mid - 1
+
+
+start = 1
+end = array[-1] - array[0]
+answer = 0
+
+binary_search(array, start, end)
+print(answer)
