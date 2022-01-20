eca73aa4571cb0fc54eab2a6da30417f9566fdb1
cho woonghyun <tofan12312@gmail.com>
2022-01-20 16:13:07 +0900

    220120 BOJ 1463 SILVER3 1로만들기 PASS(코드수정)

--- 01. 개념 및 예제 문제/DP_Example/220118_BOJ_1463_1로만들기.py
+++ 01. 개념 및 예제 문제/DP_Example/220118_BOJ_1463_1로만들기.py
@@ -1,4 +1,8 @@
 # SILVER3
+import sys
+sys.setrecursionlimit(250000)
+
+
 def solution(N):
     if N == 1 or N == 2:
         return memo[N]
@@ -7,30 +11,18 @@ def solution(N):
         return memo[N]
 
     if not N % 3:
-        tmp = solution(N // 3) + 1
-        if memo[N] > tmp:
-            memo[N] = tmp
-        # 만약 이미 기존의 최소값보다 더 큰 cnt가 나왔다면? 거기서 중지
-        else:
-            return memo[N]
+        # 다음은 min()을 사용하여 코드를 간결화한 것이다.
+        # 다만 이 코드의 경우, RecursionError가 발생한다.
+        memo[N] = min(memo[N], solution(N // 3) + 1)
     if not N % 2:
-        tmp = solution(N // 2) + 1
-        if memo[N] > tmp:
-            memo[N] = tmp
-        else:
-            return memo[N]
+        memo[N] = min(memo[N], solution(N // 2) + 1)
     if N:
-        tmp = solution(N - 1) + 1
-        if memo[N] > tmp:
-            memo[N] = tmp
-        else:
-            return memo[N]
+        memo[N] = min(memo[N], solution(N - 1) + 1)
     return memo[N]
 
 
 N = int(input())
 memo = [10000005] * 1000001
-memo[1] = 0
-memo[2] = 1
+memo[1], memo[2] = 0, 1
 solution(N)
 print(memo[N])
