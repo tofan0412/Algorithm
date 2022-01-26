3e13b54a88bd2ef706635fb71c12922a20609c44
cho woonghyun <tofan12312@gmail.com>
2022-01-26 20:27:44 +0900

    220126 BOJ 2579 SILVER3 계단오르기 PASS

--- 01. 개념 및 예제 문제/DP_Example/220120_BOJ_2579_계단오르기.py
+++ 01. 개념 및 예제 문제/DP_Example/220120_BOJ_2579_계단오르기.py
@@ -1,2 +1,22 @@
 # SILVER3
-# 풀이중
+# https://daimhada.tistory.com/181
+N = int(input())
+stairs = [] # 1,2,3,4,5,6
+for _ in range(N):
+    stairs.append(int(input()))
+
+stairs = [0] + stairs
+memo = [0] * len(stairs)
+# 첫번째 칸에 올라왔을 때 최대값은 stairs[0]이다.
+memo[1] = stairs[1]
+# 두번째 칸에 올라왔을 때 최대값은 둘 중 하나다. 연속으로 2칸 올라왔거나, 한번에 2칸 올라왔거나
+
+for stair in range(2, N+1):
+    # 직전칸에서 올라온 경우의 최댓값 / 전전칸에서 올라온 경우의 최대값
+    memo[stair] = max(stairs[stair] + stairs[stair-1] + memo[stair-3], stairs[stair] + memo[stair-2])
+
+print(memo[-1])
+
+
+
+
