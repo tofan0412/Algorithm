18d2703c461a60c416f09788fe0e223f380f7247
DESKTOP-R3600\tofan <tofan12312@gmail.com>
2022-01-28 01:42:53 +0900

    220128 BOJ 14501 SILVER3 퇴사 풀이중

--- 01. 개념 및 예제 문제/DP_Example/220120_BOJ_9095_123더하기.py
+++ 01. 개념 및 예제 문제/DP_Example/220120_BOJ_9095_123더하기.py
@@ -1,27 +1,20 @@
 # SILVER3
-def solution(n, selected):
-    # n이 0이 되었으며 A+B의 형태로 표현할 수 있다면
-    if n == 0 and len(selected) >= 1:
-        return 1
-    # n이 0이 되었지만, A+B의 형태로 표현할 수 없다면
-    result = 0
-    if n - 3 >= 0:
-        selected.append(3)
-        result += solution(n-3, selected)
-        selected.pop()
-    if n - 2 >= 0:
-        selected.append(2)
-        result += solution(n-2, selected)
-        selected.pop()
-    if n - 1 >= 0:
-        selected.append(1)
-        result += solution(n-1, selected)
-        selected.pop()
-    return result
+# n은 11보다 작다.
 
 
 T = int(input())
 for tc in range(T):
-    N = int(input())
-    print(solution(N, []))
+    memo = [0] * 11
+    memo[1] = 1
+    memo[2] = 2
+    memo[3] = 4
 
+    N = int(input())
+    num = 4
+    while True:
+        if num > N:
+            break
+        memo[num] = memo[num - 1] + memo[num - 2] + memo[num - 3]
+        num += 1
+
+    print(memo[N])
--- 01. 개념 및 예제 문제/DP_Example/220128_BOJ_14501_퇴사.py
+++ 01. 개념 및 예제 문제/DP_Example/220128_BOJ_14501_퇴사.py
@@ -1,1 +1,10 @@
 # SILVER3
+N = int(input())
+memo = [0] * (N + 1)
+t, p = [], []
+for _ in range(N):
+    time, pay = map(int, input().split())
+    t.append(time)
+    p.append(pay)
+
+print(memo[N])
