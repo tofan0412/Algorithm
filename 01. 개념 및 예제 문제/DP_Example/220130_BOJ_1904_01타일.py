5dbbf2291b071865361f31bf260a781148ab017a
DESKTOP-R3600\tofan <tofan12312@gmail.com>
2022-01-30 06:15:31 +0900

    220130 BOJ 1904 SILVER3 01타일 PASS

--- 01. 개념 및 예제 문제/DP_Example/220130_BOJ_1904_01타일.py
+++ 01. 개념 및 예제 문제/DP_Example/220130_BOJ_1904_01타일.py
@@ -1,1 +1,15 @@
 # SILVER3
+N = int(input())
+memo = [0] * 1000001
+memo[1] = 1
+memo[2] = 2
+
+num = 3
+while True:
+    if num > N:
+        break
+
+    memo[num] = (memo[num-1] + memo[num-2]) % 15746 # 나눈 값으로 저장하지 않으면 메모리 초과 발생...
+    num += 1
+
+print(memo[N])
