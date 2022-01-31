375518c70069f9b8c0c67850e764b95b1580f060
DESKTOP-R3600\tofan <tofan12312@gmail.com>
2022-01-31 23:38:23 +0900

    220131 BOJ 2407 SILVER3 조합 PASS

--- 01. 개념 및 예제 문제/DP_Example/220130_BOJ_2407_조합.py
+++ 01. 개념 및 예제 문제/DP_Example/220130_BOJ_2407_조합.py
@@ -1,2 +1,25 @@
 # SILVER3
-# nCm을 출력한다.
+# nCm을 출력한다. -> n개 중 m개 뽑기
+
+memo = [0] * 101
+n, m = map(int, input().split())
+# 공식을 풀기 -> 팩토리얼을 풀어야 한다.
+
+
+def factorial(n, origin, multiple):
+    if n == 1:
+        memo[origin] = multiple
+        return multiple
+
+    elif memo[n] != 0:
+        return multiple * memo[n]
+
+    else:
+        return factorial(n-1, origin, multiple * n)
+
+
+result = factorial(n, n, 1) // (factorial(m, m, 1) * factorial(n-m, n-m, 1))
+print(result)
+
+
+
