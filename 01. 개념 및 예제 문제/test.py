f534546cb2045c10be5048fe8526151537a6f23a
DESKTOP-R3600\tofan <tofan12312@gmail.com>
2022-02-10 13:24:57 +0900

    220210 BOJ 1174 SILVER1 줄어드는수 풀이중

--- 01. 개념 및 예제 문제/test.py
+++ 01. 개념 및 예제 문제/test.py
@@ -1,17 +1,33 @@
-T = int(input())
-for tc in range(T):
+class Friend:
+    def __init__(self, email):
+        self.email = email
+        self.friends = []
 
-    N = int(input())
+    # 각 객체의 friends라고 하는 리스트에 해당 친구의 객체를 추가한다. 
+    def add_friendship(self, friend):
+        self.friends.append(friend)
+        friend.friends.append(self)
 
-    result = ''
-    while True:
-        if N == 0:
-            break
+    def can_be_connected(self, friend):
+        # 1. Direct로 연결되어 있는 경우
+        if friend in self.friends:
+            return True
 
-        left = N % 2
-        result += str(left)
-        N = N // 2
+        # 재귀적 풀이
+        result = False
+        for other_friends in self.friends:
+            if result:
+                break
+            result = other_friends.can_be_connected(friend)
+        return result
 
-    result = "".join(reversed(result))
-    print(result)
 
+if __name__ == "__main__":
+    a = Friend("A")
+    b = Friend("B")
+    c = Friend("C")
+
+    a.add_friendship(b)
+    b.add_friendship(c)
+
+    print(a.can_be_connected(c))
