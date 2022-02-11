dff772934155a872c319096ace68f520704df09c
cho woonghyun <tofan12312@gmail.com>
2022-02-11 18:44:25 +0900

    220211 BOJ 15988 SILVER2 123더하기3 PASS

--- 01. 개념 및 예제 문제/DP_Example/220202_BOJ_2293_동전1.py
+++ 01. 개념 및 예제 문제/DP_Example/220202_BOJ_2293_동전1.py
@@ -1,18 +1,19 @@
 # GOLD5
 n, k = map(int, input().split())
 coins = []
-for _ in n:
+for _ in range(n):
     coins.append(int(input()))
 
-dp = [0] * (k + 1)
-# k부터 시작해서, n가지 Action을 취할 수 있다.
-def solution(number, route):
-    if number == 0:
+# Memoization을 위한 dp 리스트를 먼저 정의하자. dp[K]는 금액 K원에 대한 가능한 경우의 수를 뜻한다.
+dp = [0] * 10001
+
+credits = 1 # k = 1인 경우부터 시작한다.
 
 
-    if dp[number] != 0:
-        return dp[number]
+for i in range(credits, k+1):
+    # 이전 과정들이 중복되지 않았다는 가정 하에..
+    dp[i] =  
 
-    for coin in coins:
-        if number - coin > 0:
-            solution(number, route + str(coin))
+
+
+
---
+++ 01. 개념 및 예제 문제/DP_Example/220211_BOJ_15988_123더하기3.py
@@ -0,0 +1,20 @@
+# SILVER2
+t = int(input())
+dp = [0] * 1000001
+dp[1] = 1 % 1000000009
+dp[2] = 2 % 1000000009
+dp[3] = 4 % 1000000009
+dp[4] = 7 % 1000000009
+
+history = 4
+for tc in range(t):
+    n = int(input())
+
+    if n <= history:
+        print(dp[n])
+        continue
+    else:
+        for num in range(history+1, n+1):
+            dp[num] = (dp[num-1] + dp[num-2] + dp[num-3]) % 1000000009
+            history = n
+        print(dp[n])
