f008ebc9ea28ad51860c35bc33fb50beb0453154
DESKTOP-R3600\tofan <tofan12312@gmail.com>
2022-02-13 17:40:46 +0900

    220213 BOJ 1174 SILVER1 줄어드는수 PASS

--- 01. 개념 및 예제 문제/Backtracking&Divide_Example/220209_BOJ_1174_줄어드는수.py
+++ 01. 개념 및 예제 문제/Backtracking&Divide_Example/220209_BOJ_1174_줄어드는수.py
@@ -1,64 +1,31 @@
 # SILVER1
-# N번째로 작은, 줄어드는 수를 출력하는 프로그램을 작성하시오.
 import itertools
 
 N = int(input()) # N은 1부터 1,000,000보다 작거나 같은 자연수이다.
-results = []
-results.append(0)
-results.append(1)
-results.append(2)
-results.append(3)
-results.append(4)
-results.append(5)
-results.append(6)
-results.append(7)
-results.append(8)
-results.append(9)
-number = 10 # 음이 아닌 정수가 기준이다.
+arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+result = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-# 1~9C(자릿수: 2 ~ 10)
-while True:
-    if len(results) >= N or number > 9876543210:
-        break
-
-    arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    result = []
-    for number in range(2, 11):
-        # tmp는 여러 개의 튜플이 담긴 리스트이다.
-        tmp = list(itertools.combinations(arr, number))
+# 전체 경우의 수가 1024가지 밖에 되지 않기 때문에, 그냥 해도 된다.
+for length in range(2, 11):
+    numbers = list(itertools.combinations(arr, length))
+    for cand in numbers:
+        tmp = sorted(cand, reverse=True)
+        # 이제 줄어드는 수인지 확인해 보자.
+        factor = True
+        number = ''
+        for i in range(len(tmp)):
+            if i == len(tmp)-1:
+                pass
+            else:
+                if tmp[i] <= tmp[i+1]:
+                    factor = False
+            number += str(tmp[i])
+        if factor:
+            result.append(int(number))
 
 
-
-
-
-
-    num_list = list(str(number))
-    result = True
-    nono = True
-    # 1. 같은 수가 있는 경우 안된다.
-    cnt = [0] * 10
-    for i in num_list:
-        cnt[int(i)] += 1
-        if cnt[int(i)] > 1:
-            nono = False
-
-    if not nono:
-        number += 1
-        continue
-
-    for i in range(len(num_list)-1):
-        if int(num_list[i]) <= int(num_list[i+1]):
-            result = False
-            break
-
-    if result:
-        results.append(number)
-
-    number += 1
-
-if not nono:
+result = sorted(result)
+if N+1 > len(result):
     print(-1)
-elif len(results) >= N:
-    print(results[N-1])
 else:
-    print(-1)
+    print(result[N])
