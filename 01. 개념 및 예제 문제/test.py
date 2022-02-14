70260f48f031c7d70581a28e022c9fb7fca779fb
DESKTOP-R3600\tofan <tofan12312@gmail.com>
2022-02-14 21:36:08 +0900

    220214 BOJ 15990 SILVER2 123더하기5 PASS

--- 01. 개념 및 예제 문제/test.py
+++ 01. 개념 및 예제 문제/test.py
@@ -1,33 +1,55 @@
-class Friend:
-    def __init__(self, email):
-        self.email = email
-        self.friends = []
+def solution(rsp3):  # rsp3의 최대 길이는 10만
+    score = [0] * 3
+    for race in rsp3:
+        # 1. 모두 같은 경우
+        if race[0] == race[1] == race[2]:
+            continue
+        # 2. 모두 다른 경우
+        if race[0] != race[1] and race[1] != race[2] and race[0] != race[2]:
+            continue
 
-    # 각 객체의 friends라고 하는 리스트에 해당 친구의 객체를 추가한다. 
-    def add_friendship(self, friend):
-        self.friends.append(friend)
-        friend.friends.append(self)
+        # 이외의 경우는 2개는 같고, 하나만 다른 경우이다. => 6가지
+        rsp = [0] * 3  # R, S, P
+        for s in race:
+            if s == 'R':
+                rsp[0] += 1
+            elif s == 'S':
+                rsp[1] += 1
+            else:
+                rsp[2] += 1
 
-    def can_be_connected(self, friend):
-        # 1. Direct로 연결되어 있는 경우
-        if friend in self.friends:
-            return True
+        winner = ''
+        # 승자는 바위
+        if rsp == [2, 1, 0] or rsp == [1, 2, 0]:
+            winner = 'R'
+        # 승자는 보
+        elif rsp == [2, 0, 1] or rsp == [1, 0, 2]:
+            winner = 'P'
+        # 승자는 가위
+        elif rsp == [0, 2, 1] or rsp == [0, 1, 2]:
+            winner = 'S'
 
-        # 재귀적 풀이
-        result = False
-        for other_friends in self.friends:
-            if result:
-                break
-            result = other_friends.can_be_connected(friend)
-        return result
+        winner_idx = []
+        for i in range(len(race)):
+            if race[i] == winner:
+                winner_idx.append(i)
+
+        # 1명이 이긴 경우에는 2점
+        if len(winner_idx) == 1:
+            score[winner_idx[0]] += 2
+        # 2명이 이긴 경우에는 누적 점수를 비교해야 한다.
+        else:
+            if score[winner_idx[0]] == score[winner_idx[1]]:
+                score[winner_idx[0]] += 1
+                score[winner_idx[1]] += 1
+            else:
+                # 현재 누적 점수가 다른 사람이 final_winner
+                if score[winner_idx[0]] > score[winner_idx[1]]:
+                    score[winner_idx[1]] += 2
+                elif score[winner_idx[0]] < score[winner_idx[1]]:
+                    score[winner_idx[0]] += 2
+    return score
 
 
-if __name__ == "__main__":
-    a = Friend("A")
-    b = Friend("B")
-    c = Friend("C")
+print(solution(["SSP", "PPS", "SSP", "RPP", "PRP"]))
 
-    a.add_friendship(b)
-    b.add_friendship(c)
-
-    print(a.can_be_connected(c))
