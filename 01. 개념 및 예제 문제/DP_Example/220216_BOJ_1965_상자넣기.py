e37f4c1aa714dfb04a781d73e64273f72780ca0f
DESKTOP-R3600\tofan <tofan12312@gmail.com>
2022-02-16 01:51:06 +0900

    220216 BOJ 1965 SILVER2 상자넣기 PASS

--- 01. 개념 및 예제 문제/DP_Example/220216_BOJ_1965_상자넣기.py
+++ 01. 개념 및 예제 문제/DP_Example/220216_BOJ_1965_상자넣기.py
@@ -1,1 +1,14 @@
 # SILVER2
+# 최장 증가 부분 수열 구하라는 거잖아...
+n = int(input())
+arr = list(map(int, input().split()))
+
+# 1. dp[k] 정의 : arr[k]를 포함하는 LIS의 길이
+dp = [1] * n #1로 하는 이유: 자기 자신부터 시작할 경우도 있기 때문에...
+
+for i in range(n):
+    for j in range(i):
+        if arr[i] > arr[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+print(max(dp))
+
