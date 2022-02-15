cb58e72411c7b7eb2d34d56aeaa8fd6bde8329ac
DESKTOP-R3600\tofan <tofan12312@gmail.com>
2022-02-16 03:30:07 +0900

    220216 BOJ 9655 SILVER5 돌게임 DP풀이 PASS

--- 01. 개념 및 예제 문제/DP_Example/220208_BOJ_9655_돌게임.py
+++ 01. 개념 및 예제 문제/DP_Example/220208_BOJ_9655_돌게임.py
@@ -2,25 +2,20 @@
 # 탁자 위에 돌 N개가 있다.
 # 상근이와 창영이는 턴을 번갈아 가면서 돌을 가져가며, 돌은 1개 or 3개 가져갈 수 있다.
 # 마지막 돌을 가져가는 사람이 이기게 된다.
-memo = [''] * 1001 # 돌이 N개 있을 때, 이기는 사람을 기록해 둔다.
-N = int(input())
-memo[1] = 'SK' # 1개 있으므로, 무조건 상근이가 이긴다.
-memo[2] = 'CY'
-memo[3] = 'SK'
-memo[4] = 'CY'
 
-stones = 5
+n = int(input())
+dp = [['', ''] for _ in range(n+1)]
 
-# 상근이와 창영이는 본인의 차례에 돌을 1개 빼거나, 3개 빼는 행동만을 할 수 있다.
-while True:
-    if stones > N:
-        break
+# 상근이 차례에, 남은 돌의 개수가 0개 일때
+dp[0][0] = 'CY' # 상근이 차례에 남은 돌의 개수가 0개
+dp[0][1] = 'SK' # 창영이 차례에 남은 돌의 개수가 0개
+dp[1][0] = 'SK'
+dp[1][1] = 'CY'
 
-    if memo[stones-1] == memo[stones-3] == 'SK':
-        memo[stones] = 'CY'
-    elif memo[stones-1] == memo[stones-3] == 'CY':
-        memo[stones] = 'SK'
+for i in range(2, n+1):
+    # 남은 돌의 개수가 i개이고, 차례가 상근이일 때
+    dp[i][0] = dp[i-1][1]
+    # 남은 돌의 개수가 i개이고, 차례가 창영일 때
+    dp[i][1] = dp[i-1][0]
 
-    stones += 1
-print(memo[N])
-
+print(dp[n][0])
