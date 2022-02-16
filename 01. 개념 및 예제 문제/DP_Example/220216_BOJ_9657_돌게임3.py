fb78aeb401e3c367433c0eff9491b713bb15c436
DESKTOP-R3600\tofan <tofan12312@gmail.com>
2022-02-17 01:01:53 +0900

    220217 BOJ 9657 SILVER3 돌게임3(내풀이) PASS

--- 01. 개념 및 예제 문제/DP_Example/220216_BOJ_9657_돌게임3.py
+++ 01. 개념 및 예제 문제/DP_Example/220216_BOJ_9657_돌게임3.py
@@ -1,35 +1,30 @@
 # SILVER3
 # 완벽하게 게임을 한다는 건, 이길 수 있는 경우의 수가 있다면 그 경우의 수만 놓는다는 의미이다.
-
 n = int(input())
-dp = ['' for _ in range(1001)]
-dp[1] = 'SK'
-dp[2] = 'CY'
-dp[3] = 'SK'
-dp[4] = 'SK'
-dp[5] = 'SK'
-dp[6] = 'SK'
-dp[7] = 'CY'
-dp[8] = 'SK'
 
-for i in range(9, n+1):
-    # SK이가 승리할 수 있는 경우의 수가 하나라도 있다면
+# dp[i]의 정의 : 상근이든 창영이든, 내 차례에 남은 돌의 개수가 i개일 때 내가 이길 수 있는지의 여부를 나타낸다(한 가지 경우라도 내가 이길 수 있는 경우의 수가 있다면 1이다).
+# 1은 이길 수 있는 것이고 0은 이길 수 없는 것이다.
+dp = [0 for _ in range(1001)]
 
-    # 상근이가 먼저 시작하므로, dp[i-1], dp[i-3], dp[i-4]에서 차례는 창영이다.
-    # 1. 상근이가 i개의 돌에서 1개를 뺸 경우 -> 창영이는 i-1개의 돌에서 1,3,4개를 뺄 수 있다.
-    case1 = i - 1
-    case2 = i - 3
-    case3 = i - 4
-    # 1,3,4개를 뺀 이후 남은 돌의 dp[]를 판단해야 한다.
-    if dp[case1-1] == 'SK' or dp[case1-3] == 'SK' or dp[case1-4] == 'SK':
-        dp[i] = 'SK'
-    # 2. 상근이가 i개의 돌에서 3개를 뺸 경우
-    elif dp[case2-1] == 'SK' or dp[case2-3] == 'SK' or dp[case2-4] == 'SK':
-        dp[i] = 'SK'
-    # 3. 상근이가 i개의 돌에서 4개를 뺸 경우
-    elif dp[case3-1] == 'SK' or dp[case3-3] == 'SK' or dp[case3-4] == 'SK':
-        dp[i] = 'SK'
-    else:
-        dp[i] = 'CY'
+dp[1] = 1
+dp[2] = 0 # 내 차례에 돌이 2개 있다면 절대 이길 수 없음
+dp[3] = 1
+dp[4] = 1
 
-print(dp[n])
+for stone in range(5, n+1):
+    # 만약 상대방이 상대방 차례에 stone-1개의 돌을 가지고 있을 때 상대방이 이길 수 있는 경우의 수가 0개라면? 내가 이기는 것이다. (모든 경우가 자신이 이기는 경우이기 때문에)
+    if not dp[stone-1]:
+        dp[stone] = 1
+    if not dp[stone-3]:
+        dp[stone] = 1
+    if not dp[stone-4]:
+        dp[stone] = 1
+
+if dp[n] == 1:
+    print('SK') # 시작은 상근이가 하기 때문에
+else:
+    print('CY')
+
+
+
+
