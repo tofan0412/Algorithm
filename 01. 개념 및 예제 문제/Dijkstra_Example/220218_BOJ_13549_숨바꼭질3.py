9c1d1c922f1522f64d4b417117566e120e7319e8
cho woonghyun <tofan12312@gmail.com>
2022-02-18 15:08:54 +0900

    220218 BOJ 13549 GOLD5 숨바꼭질3 PASS

--- 01. 개념 및 예제 문제/Dijkstra_Example/220218_BOJ_13549_숨바꼭질3.py
+++ 01. 개념 및 예제 문제/Dijkstra_Example/220218_BOJ_13549_숨바꼭질3.py
@@ -1,15 +1,7 @@
+import heapq
 # GOLD5
-# 수빈이는 현재 점 N에 있고, 동생은 점 K에 있다. (0 <= N, K <= 100,000)
-# 수빈이의 위치가 X일 때 걷는다면 1초 후에 X-1 또는 X+1로 이동하게 된다.
-# 순간 이동을 하는 경우에는 0초 후에 2*X의 위치로 이동하게 된다.
-
-# 수빈이와 동생의 위치가 주어졌을 때, 수빈이가 동생을 찾을 수 있는 가장 빠른 시간이 몇 초후일지 구하시오
-
-INF = int(1e9) # 무한을 나타내는 수. 실제로는 10^9이다.
+INF = int(1e9)
 n, k = map(int, input().split())
-
-# BFS 또는 다익스트라 알고리즘을 이용하여 풀 수 있다.
-
 # 1. 다익스트라로 풀려고 한다면?
 # 이 경우 한 노드에 대한 인접 정보를 제공하는 graph는 필요없다. -> n과 2n에 대한 간선, n - n+1, n - n-1에 대한 간선 정보로 고정
 # 도시(좌표)의 개수가 최대 10만개이므로, O(N^2) 풀이가 아닌 O(ElogV) 풀이를 통해 접근하자.
@@ -17,6 +9,32 @@ n, k = map(int, input().split())
 distance = [INF] * 100001
 
 
+def dijkstra(start):
+    # 1. 기준 노드에 대해 처리한다.
+    q = []
+    heapq.heappush(q, (0, start)) # 튜플의 0번째는 해당 노드에 방문하기 위해 필요한 비용, 1번째는 노드번호(=좌표)이다.
+    distance[start] = 0
+
+    while q:
+        # 인접 노드 조사. 이때 인접노드는 3가지이다. n-1, n+1, 2n
+        dist, now = heapq.heappop(q)
+
+        # 만약 이미 방문한 곳이라면? 다음으로..
+        if distance[now] < dist:
+            continue
+        adj = [now-1, now+1, now*2]
+        cost_arr = [1, 1, 0]
+        for i in range(3):
+            if 0 <= adj[i] < 100001:
+                cost = distance[now] + cost_arr[i]
+                if cost < distance[adj[i]]:
+                    distance[adj[i]] = cost
+                    heapq.heappush(q, (distance[adj[i]], adj[i]))
+
+
+dijkstra(n)
+
+print(distance[k])
 
 
 
