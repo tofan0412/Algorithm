9e5d734e0dce107574b2345bcea62298a4b2aecf
DESKTOP-R3600\tofan <tofan12312@gmail.com>
2022-03-05 02:36:27 +0900

    220305 BOJ 14225 SILVER1 부분수열의합 PASS

--- 01. 개념 및 예제 문제/BruteForce_Example/220305_BOJ_14225_부분수열의합.py
+++ 01. 개념 및 예제 문제/BruteForce_Example/220305_BOJ_14225_부분수열의합.py
@@ -1,23 +1,27 @@
 # SILVER1
-def solution(s, i, total): # s는 배열, i는 인덱스, total은 합이다.
-    global result
-    if i == len(s):
-        if total not in result:
-            result.append(total)
-        return
-
-    solution(s, i+1, total + s[i])
-    solution(s, i+1, total)
+# 시간 초과가 발생하니까, combination 쓰자
+from itertools import combinations
+from collections import Counter
 
 
 n = int(input())
 s = list(map(int, input().split()))
+
 result = []
+for i in range(1, n+1):
+    comb = list(combinations(s, i))
+    for j in comb:
+        result.append(sum(j))
 
-solution(s, 0, 0)
 result = sorted(result)
-result.pop(0)
+num_list = [i for i in range(1, result[-1]+2)]
 
-# 이제 가장 작은 자연수를 찾자.
+# 여기서 시간초과 발생
+# for num in num_list:
+#     if num not in result:
+#         print(num)
+#         break
 
 
+fin = Counter(num_list) - Counter(result)
+print(list(fin.keys())[0])
