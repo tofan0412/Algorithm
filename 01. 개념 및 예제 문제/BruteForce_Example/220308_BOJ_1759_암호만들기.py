c180b17f6d35af5d38628f48d87c60e044610a4a
cho woonghyun <tofan12312@gmail.com>
2022-03-08 17:16:29 +0900

    220308 BOJ 1759 GOLD5 암호만들기 풀이중

--- 01. 개념 및 예제 문제/BruteForce_Example/220308_BOJ_1759_암호만들기.py
+++ 01. 개념 및 예제 문제/BruteForce_Example/220308_BOJ_1759_암호만들기.py
@@ -1,49 +1,65 @@
-from itertools import permutations
+from itertools import permutations, combinations
+
+def check_order(s):
+    correct = True
+    for i in range(len(s)-1):
+        if total_alphabet.index(s[i]) > total_alphabet.index(s[i+1]):
+            correct = False
+    return correct
+
+
 # GOLD5
 gather = ['a', 'e', 'i', 'o', 'u']
 consonant = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
 total_alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y,' 'z']
 
-# c는 사용했을 법한 문자의 종류, l은 암호의 길이를 뜻한다.
+# permutations 쓰면 메모리 초과 발생한다..
+# l은 암호의 길이, c는 후보 문자의 길이를 뜻한다.
 l, c = map(int, input().split())
 alphabets = list(map(str, input().split()))
 
-# 1. c개의 문자 중에서 l개의 문자를 뽑는 모든 경우의 수
-candidates = list(permutations(alphabets, l))
+# 전략) 1개의 모음, 2개의 자음을 선택하고 l - 3만큼 남는 자리에 남는 문자를 넣자.
+cand_gather, cand_conso = [], []
+for a in alphabets:
+    if a in gather:
+        cand_gather.append(a)
+    else:
+        cand_conso.append(a)
 
-tmp = []
-# 2. 각 요소에 대해 1개의 모음, 2개의 자음을 포함하고 있는지 검사
-for cand in candidates:
-    # cand는 튜플이다.
-    num_gather = 0
-    num_consonant = 0
-    for alpha in cand:
-        if alpha in gather:
-            num_gather += 1
-        elif alpha in consonant:
-            num_consonant += 1
+# 1. 모음 선택
 
-    if num_gather > 0 and num_consonant > 1:
-        tmp.append(cand)
+answer = []
+for gather in cand_gather:
+    # 2. 자음 2개 선택
+    consos = list(combinations(cand_conso, 2))
 
+    for conso in consos:
+        tmp = list(alphabets)
+        result = [gather, conso[0], conso[1]] # 3개는 뽑았다.
+        tmp.remove(gather)
+        tmp.remove(conso[0])
+        tmp.remove(conso[1])
 
-result = []
-for cand in tmp:
-    is_candidate = True
-    for i in range(len(cand)-1):
-        if total_alphabet.index(cand[i]) > total_alphabet.index(cand[i+1]):
-            is_candidate = False
+        # 이제 tmp에서 자음, 모음 상관없이 l - 3개만큼 뽑으면 된다.
+        candidates = list(combinations(tmp, l - 3))
+        for cand in candidates:
+            cand = list(cand)
+            result_tmp = list(result)
+            result_tmp = result_tmp + cand
 
-    if is_candidate:
-        result.append(cand)
-
-for i in result:
-    result_str = ''
-    for j in i:
-        result_str += str(j)
-    print(result_str)
+            # 모두 뽑았으므로 이제 순서에 맞는 애들만 생각해보자.
+            tmp = list(permutations(result_tmp, l))
+            for case in tmp:
+                is_correct = check_order(case)
+                if is_correct:
+                    if case not in answer:
+                        answer.append(case)
 
 
-
+for ans in answer:
+    ans_str = ''
+    for s in ans:
+        ans_str += str(s)
+    print(ans_str)
 
