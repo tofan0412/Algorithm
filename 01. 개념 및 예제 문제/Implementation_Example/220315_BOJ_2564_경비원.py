81bfce5d1bf643b02c4bad6012da89e09e263a43
cho woonghyun <tofan12312@gmail.com>
2022-03-15 20:02:53 +0900

    220315 BOJ 2564 SILVER1 경비원 PASS

--- 01. 개념 및 예제 문제/Implementation_Example/220315_BOJ_2564_경비원.py
+++ 01. 개념 및 예제 문제/Implementation_Example/220315_BOJ_2564_경비원.py
@@ -1,6 +1,60 @@
 # SILVER1
 width, height = map(int, input().split())
 cnt_store = int(input())
-# 1. 굳이 2차원으로 만들 필요가 없다. -> 원형 Queue를 사용해보자.
 
-# 2. 상점을 표시하자.
+route = [0 for _ in range((width + height) * 2)]
+store_indexs = []
+for _ in range(cnt_store):
+    way, idx = map(int, input().split())
+    if way == 1: # 북쪽인 경우
+        route[idx] = 1
+        store_indexs.append(idx)
+    if way == 2: # 남쪽인 경우
+        route[width + height + (width - idx)] = 1
+        store_indexs.append(width + height + (width - idx))
+    if way == 3:
+        route[width + height + width + (height - idx)] = 1
+        store_indexs.append(width + height + width + (height - idx))
+    if way == 4:
+        route[width + idx] = 1
+        store_indexs.append(width + idx)
+
+# 마지막으로 경비 위치 표시
+way, idx = map(int, input().split())
+std_point = 0
+if way == 1:
+    route[idx] = 2
+    std_point = idx
+if way == 2:
+    route[width + height + (width - idx)] = 2
+    std_point = width + height + (width - idx)
+if way == 3:
+    route[width + height + width + (height - idx)] = 2
+    std_point = width + height + width + (height - idx)
+if way == 4:
+    route[width + idx] = 2
+    std_point = width + idx
+
+# 이제 세면 된다.
+result = 0
+for store in store_indexs:
+    if store < std_point:
+        # 1. 시계방향으로 재보기
+        dist1 = std_point - store
+        # 2. 반시계 방향으로 재보기
+        dist2 = store + (len(route) - std_point)
+        result += min(dist1, dist2)
+    elif store > std_point:
+        # 1. 시계방향으로 재기
+        dist1 = (len(route) - store) + std_point
+        # 2. 반시계 방향으로 재기
+        dist2 = store - std_point
+        result += min(dist1, dist2)
+
+print(result)
+
+
+
+
+
+
