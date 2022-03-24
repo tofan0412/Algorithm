23417dae5e821ffa7f6f26bc8820abcfe12e240d
DESKTOP-R3600\tofan <tofan12312@gmail.com>
2022-03-24 18:39:08 +0900

    220324 BOJ 5639 GOLD5 이진검색트리 풀이중

--- 01. 개념 및 예제 문제/BinarySearch_Example/220324_BOJ_5639_이진검색트리.py
+++ 01. 개념 및 예제 문제/BinarySearch_Example/220324_BOJ_5639_이진검색트리.py
@@ -1,1 +1,28 @@
 # GOLD5
+'''
+이진 검색 트리는 다음과 같은 세 가지 조건을 만족하는 이진트리이다.
+노드의 왼쪽 서브트리에 있는 모든 노드의 키는 노드의 키보다 작다
+노드의 오른쪽 서브트리에 있는 모든 노드의 키는 노드의 키보다 크다.
+
+전위 순회는 루트 - 왼쪽 - 오른쪽
+후위 순회는 왼쪽 오른쪽 루트
+
+
+'''
+
+
+class Node:
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.value = value
+
+
+class BST:
+    def __init__(self, root):
+        self.root = root
+
+    def insert(self, value):
+        self.current_node = self.node
+
+
