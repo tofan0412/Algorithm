b74f32910cbd54edecdd7252b5e0de54693a4665
DESKTOP-R3600\tofan <tofan12312@gmail.com>
2022-04-02 03:07:56 +0900

    220402 BOJ 3020 GOLD5 개똥벌레 풀이중

--- 01. 개념 및 예제 문제/BinarySearch_Example/220402_BOJ_3020_개똥벌레.py
+++ 01. 개념 및 예제 문제/BinarySearch_Example/220402_BOJ_3020_개똥벌레.py
@@ -1,16 +1,48 @@
-# GOLD5
-width, height = map(int, input().split()) # 이 때 n은 항상 짝수이다.
+import sys
+
+def binary_search(arr, start, end): # start는 구간의 시작 높이값, end는 구간의 종료 높이를 뜻한다.
+    global height, cases
+
+    # 종료 조건을 설정해야 한다.
+    if start >= end:
+        return
+
+    mid = (start + end) // 2
+    criterion = mid + 0.5
+
+    cnt = 0
+    for i, obs in enumerate(arr):
+        # 1. i가 짝수인 경우 석순이고
+        if i % 2 == 0:
+            if obs > criterion:
+                cnt += 1
+        # 2. i가 홀수인 경우 종유석이다.
+        else:
+            if criterion > (height - obs):
+                cnt += 1
+
+    cases.append(cnt)
+    # 모든 cnt는 기록해 둔다. 단 이진탐색을 통해 탐색하고 있으므로, h의 높이에 대해 모든 구간을 확인하는 건 절대 아니다.
+    # 아래 방향 탐색
+    binary_search(arr, start, mid)
+    binary_search(arr, mid+1, end)
+
+
+width, height = map(int, sys.stdin.readline().rstrip().split())
 obstacles = []
 for _ in range(width):
-    obstacles.append(int(input()))
+    obstacles.append(int(sys.stdin.readline().rstrip()))
 
-# 주의해야 할 건, 짝수번째 index에 있는 대상은 석순의 높이이고,
-# 홀수번째 인덱스에 있는 대상은 종유석의 깊이이다.
+cases = []
+binary_search(obstacles, 0, height)
 
-# 특정 높이를 선택해야 한다.
-# 높이가 h일 때, 구간 또한 h개가 존재한다.
+# 이제 케이스에서 가장 min값을 찾고, 해당 min값이 몇번 출현했는지 세본다.
+cases.sort()
+min_val = cases[0]
+result = 0
+for case in cases:
+    if case != min_val:
+        break
+    result += 1
 
-def binary_search():
-    
-
-
+print(str(min_val) + " " + str(result))
