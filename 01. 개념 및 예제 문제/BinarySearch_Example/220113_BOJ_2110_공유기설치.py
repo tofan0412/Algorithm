f580892bca0a87f269eed26f7782bddfe8bb0322
DESKTOP-R3600\tofan <tofan12312@gmail.com>
2022-04-02 04:32:43 +0900

    220402 BOJ 2110 GOLD5 공유기설치 PASS

--- 01. 개념 및 예제 문제/BinarySearch_Example/220113_BOJ_2110_공유기설치.py
+++ 01. 개념 및 예제 문제/BinarySearch_Example/220113_BOJ_2110_공유기설치.py
@@ -1,37 +1,45 @@
+import sys
 # GOLD5
-# 참고 코드 : https://hongcoding.tistory.com/3
-N, C = map(int, input().split())
+def binary_search(arr, start, end): # start는 첫번째 집의 좌표, end는 마지막 집의 좌표
+    global result, C
 
-array = []
-for i in range(N):
-    array.append(int(input()))
+    if start > end:
+        return
 
-array.sort()
+    mid = (start + end) // 2 # 기준이 될 거리값이다.
+
+    # 첫번째 집에 설치한다.
+    visited = [False] * N
+    visited[0] = True
+    cnt = 1 # 공유기를 설치한 집의 개수
+    last_i = 0 # 마지막으로 공유기를 설치한 집의 인덱스 번호
+    # 1. target 거리 혹은 그 이상으로 모든 집을 배치할 수 있는가?
+    for i in range(1, len(arr)):
+        if cnt == C: # 더이상 설치할 수 없음.
+            break
+        # 1-1. 마지막으로 공유기를 설치한 집과의 거리 차이가 mid 이상이면 섫치한다.
+        if arr[i] - arr[last_i] >= mid and not visited[i]:
+            last_i = i
+            cnt += 1
+            visited[i] = True
+    # 2. 이 과정 이후 False가 visited에 없다면? Target 거리 이상으로 공유기를 설치할 수 있다는 뜻이다.
+    if cnt == C:
+        # 해당 거리값을 기록해 둔다.
+        if result < mid:
+            result = mid
+        binary_search(arr, mid+1, end)
+    else:
+        binary_search(arr, start, mid-1)
 
 
-def binary_search(array, start, end):
-    while start <= end:
-        mid = (start + end) // 2
-        current = array[0] # 현재 설치한 집을 뜻한다.
-        count = 1 # 공유기를 설치한 집 수
+N, C = map(int, sys.stdin.readline().rstrip().split())
+arr = sorted([int(sys.stdin.readline()) for _ in range(N)])
 
-        # 두번째 집부터 마지막 집까지 점건한다.
-        for i in range(1, len(array)):
-            if array[i] >= current + mid:
-                count += 1
-                current = array[i]
+result = -int(1e9)
+max_dist = arr[-1] - arr[0]
+binary_search(arr, 1, max_dist)
 
-        if count >= C:
-            global answer
-            start = mid + 1
-            answer = mid
-        else:
-            end = mid - 1
+print(result)
 
 
-start = 1
-end = array[-1] - array[0]
-answer = 0
 
-binary_search(array, start, end)
-print(answer)
