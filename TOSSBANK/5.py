4a21ae1a0255dbc0f94a06553dcebf1280a9fd72
DESKTOP-R3600\tofan <tofan12312@gmail.com>
2022-04-01 10:28:31 +0900

    220401 TOSSBANK 직사각형 풀이중

--- TOSSBANK/5.py
+++ TOSSBANK/5.py
@@ -7,8 +7,10 @@ def bfs(q, arr, v): # max (row, col)과 min (row, col)을 반환한다.
     dr = [-1, 1, 0, 0]
     dc = [0, 0, -1, 1]
 
-    max_info = [q[0][0], q[0][0]]
-    min_info = [q[0][0], q[0][0]]
+    max_row = q[0][0]
+    min_row = q[0][0]
+    max_col = q[0][1]
+    min_col = q[0][1]
 
     while q:
         now = q.popleft()
@@ -24,30 +26,40 @@ def bfs(q, arr, v): # max (row, col)과 min (row, col)을 반환한다.
                     q.append((gr, gc))
                     v[gr][gc] = True
 
-                    # 1. min 정보 검사
-                    if gr < min_info[0] and gc < min_info[1]:
-                        min_info = [gr, gc]
-                    # 2. max 정보 검사
-                    elif max_info[0] < gr and max_info[1] < gc:
-                        max_info = [gr, gc]
-    return max_info, min_info
+                    if gr > max_row:
+                        max_row = gr
+                    elif gr < min_row:
+                        min_row = gr
+
+                    if gc > max_col:
+                        max_col = gc
+                    elif gc < min_col:
+                        min_col = gc
+
+    return max_row, max_col, min_row, min_col
 
 
 def solution(arr):
-    for case in arr:
+    for i, case in enumerate(arr):
+        print("현재 케이스는 " + str(i + 1))
         height = len(case)
         width = len(case[0])
 
         for row in range(height):
             for col in range(width):
                 # 만약 흰색 타일인 경우 완탐 실시
-                if arr[row][col] == '.':
+                if case[row][col] == '.':
                     queue = deque()
                     queue.append((row, col))
                     visited = [[False] * width for _ in range(height)]
                     visited[row][col] = True
 
-                    r1, r2 = bfs(queue, arr, visited) # min_info와 max_info를 반환하게 된다.
+                    maxr, maxc, minr, minc = bfs(queue, case, visited) # min_info와 max_info를 반환하게 된다.
+                    print("제일 위쪽 : " + str(minr), end=" ")
+                    print("제일 아래쪽 : " + str(maxr), end=", ")
+                    print("제일 왼쪽 : " + str(minc), end=" ")
+                    print("제일 오른쪽 : " + str(maxc), end=" ")
+                    print()
 
 
 solution([[".....", ".XXX.", ".X.X.", ".XXX.", "....."],
@@ -56,3 +68,8 @@ solution([[".....", ".XXX.", ".X.X.", ".XXX.", "....."],
           ["....X", ".....", "XXX..", "X.X..", "XXX.."],
           [".......", "XXX.XXX", "X.X.X.X", "XXX.XXX", "......."],
           ["XXXXX", "XX.XX", "X...X", "XX.XX", "XXXXX"]])
+
+
+# solution([["XXXXX", "XXXXX", "XXX.X", "XXX.X", "XXXXX"]])
+
+# solution([["XXXXX", "XX.XX", "X...X", "XX.XX", "XXXXX"]])
