482c0bc1b3effe22514e48d37dcd33d85ee3b442
DESKTOP-R3600\tofan <tofan12312@gmail.com>
2022-04-02 03:42:20 +0900

    220402 BOJ 3020 GOLD5 개똥벌레 풀이중

--- 01. 개념 및 예제 문제/BinarySearch_Example/220402_BOJ_3020_개똥벌레.py
+++ 01. 개념 및 예제 문제/BinarySearch_Example/220402_BOJ_3020_개똥벌레.py
@@ -1,48 +1,43 @@
 import sys
 
-def binary_search(arr, start, end): # start는 구간의 시작 높이값, end는 구간의 종료 높이를 뜻한다.
-    global height, cases
 
-    # 종료 조건을 설정해야 한다.
+def binary_search(arr, start, end, target):
     if start >= end:
-        return
+        return start
 
     mid = (start + end) // 2
-    criterion = mid + 0.5
 
-    cnt = 0
-    for i, obs in enumerate(arr):
-        # 1. i가 짝수인 경우 석순이고
-        if i % 2 == 0:
-            if obs > criterion:
-                cnt += 1
-        # 2. i가 홀수인 경우 종유석이다.
-        else:
-            if criterion > (height - obs):
-                cnt += 1
-
-    cases.append(cnt)
-    # 모든 cnt는 기록해 둔다. 단 이진탐색을 통해 탐색하고 있으므로, h의 높이에 대해 모든 구간을 확인하는 건 절대 아니다.
-    # 아래 방향 탐색
-    binary_search(arr, start, mid)
-    binary_search(arr, mid+1, end)
+    if arr[mid] <= target:
+        binary_search(arr, mid + 1, end, target)
+    else:
+        binary_search(arr, start, mid, target)
 
 
 width, height = map(int, sys.stdin.readline().rstrip().split())
 obstacles = []
-for _ in range(width):
-    obstacles.append(int(sys.stdin.readline().rstrip()))
+up = [] # 종유석
+down = [] # 석순
+for i in range(width):
+    if i % 2 == 0:
+        down.append(int(sys.stdin.readline().rstrip()))
+    else:
+        up.append(height - int(sys.stdin.readline().rstrip())) # 특정 높이 h에 대해 이 값보다 크다면 무조건 충돌한다.
 
-cases = []
-binary_search(obstacles, 0, height)
+# 이진 탐색의 기본 전제 조건은 정렬된 상태이다.
+# 기존 코드의 문제점 : h에 대해 그냥 완탐을 해버림.. h는 특정 기준값이 없어 절반만 선택하기 애매하다.
 
-# 이제 케이스에서 가장 min값을 찾고, 해당 min값이 몇번 출현했는지 세본다.
-cases.sort()
-min_val = cases[0]
+up.sort()
+down.sort()
+
+min_val = 0
 result = 0
-for case in cases:
-    if case != min_val:
-        break
-    result += 1
 
-print(str(min_val) + " " + str(result))
+# h에 대해선 그냥 linear하게 간다.
+for h in range(1, height+1):
+    # 1. 종유석에 대해 높이가 h일 때 카운트 해보자. h는 0, 1, 2, ... height이다.
+    cnt1 = binary_search(down, 0, len(down), h)
+    cnt2 = binary_search(up, 0, len(up), h-1)
+
+
+
+
